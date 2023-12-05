fcd584e84aa7f8a56bdcfceb25fedd0f479ac22a
James Marghella[D[D[D[D[D[D[D[D[D[D[D[i[C[3~[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C Marghella <j.marghella@gmail.com>
2023-12-05 14:47:19 -0500

    landing page stuff

--- vue/src/views/LandingView.vue
+++ vue/src/views/LandingView.vue
@@ -6,17 +6,26 @@
 -->
 
 <template>
+
+  <nav>
+    <div id="loginDiv">
+    <button type="button" id="loginBtn" @click="login">Login</button>
+  
+    <button type="button" id="registerBtn" @click="newAccount" >Register</button></div>
+  </nav>
+
+  <p id="appName">Tee Time N'at</p>
+  
   <div id="div1">
-    <p id="AppName">Tee Time N'at</p>
+    
     <img class="logo2" src="../assets\Tee Time N'At (Ya JAG)_2 (1).png" alt="">
   </div>
-  <div id="loginDiv">
-    <button type="button" id="loginBtn" @click="login">Login</button></div>
-  <div id="registerBtn"><button>Register</button></div>
   <div class="api"><textarea name="" id="" cols="0" rows="1" placeholder="Search Courses"></textarea></div>
+
     <div></div>
 </template>
 <script>
+import RegisterView from './RegisterView.vue';
 import LoginView from './LoginView.vue';
 
 export default {
@@ -27,12 +36,46 @@ export default {
   },
   methods: {
     login() {
-      LoginView
+      this.$router.push({name: 'login'});
+    },
+    newAccount() {
+      this.$router.push({name: 'register'})
     }
   },
   
 };
 </script>
-<style>
 
+<style scoped>
+nav {
+  display: flex;
+  justify-content: flex-end;
+}
+
+#div1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 2rem;
+}
+
+.api {
+  display: flex;
+  justify-content: center;
+  padding-top: 1rem;
+}
+
+body {
+  background-color: rgb(67, 67, 66);
+}
+
+p {
+  display: flex;
+  justify-content: center;
+  
+  color: rgb(244, 224, 7);
+  font-size: x-large;
+  font-family: cursive;
+  
+}
 </style>
