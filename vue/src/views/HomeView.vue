d6b028479a29e0c109e81c9faf76ad5fb51622bc
Benjamin Hoffman <benblazer2020@gmail.com>
2023-12-06 11:58:00 -0500

    lol

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -3,7 +3,6 @@
     <h1>Home</h1>
   </div>
 
-<<<<<<< HEAD
   <nav>
     <!-- leaderboard stuff -->
 
@@ -22,9 +21,6 @@
   </div>
 
 <!-- TODO: Include links/show:
-=======
-  <!-- TODO: Include links/show:
->>>>>>> bf13accb44c46b73325d2c7232e5ae3fe9674a0a
   - leader board
   - tee-times
   - create a new league
