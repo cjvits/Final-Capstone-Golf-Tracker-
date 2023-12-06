91bb51443d7afcbda074e893f1bf0b056c78f163
Meg Lemon <meganmadalynn@gmail.com>
2023-12-06 15:41:55 -0500

    home view edits

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,28 +1,118 @@
+
 <template>
-  <div class="home">
-    <h1>Welcome to the Home View, ya Jag!</h1>
-    <Home />
-    <!-- HomeView specific shit here... -->
-  </div>
+<div class="home-container">
 
-  <nav>
-    <!-- leaderboard stuff -->
+<div class="header">
+  <h2>Welcome Home Ya Golf'n Yinzer!</h2>
+</div>
 
-    <!-- tee times stuff -->
+<div class="row">
+  <div class="left-column">Left Column</div>
+  <div class="center-column">Center Column</div>
+  <div class="right-column">Right Column</div>
+</div>
 
-    <!-- creating a new league -->
+<div class="footer">
+  <p>
+    <img src="https://media.giphy.com/media/Moo8SpSUk6R4A/giphy.gif" />
+  </p>
+</div>
+</div>
+</template>
 
-    <!-- admin stuff IF ADMIN -->
 
-    <!-- logout -->
-    <button v-on:click="logOut">Log Out</button>
-  </nav>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
 
-  <div>
-    <!-- my upcoming -->
-  </div>
+/* Style the header */
+.header {
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+}
 
-  <!-- TODO: Include links/show:
+/* Container for flexboxes */
+.row {
+  display: -webkit-flex;
+  display: flex;
+}
+
+/* Create three unequal columns that sits next to each other */
+.row {
+  padding: 10px;
+  height: 50%; /* adjust to auto after things are in them! */
+}
+
+/* Left and right column */
+.right-column {
+   -webkit-flex: 1;
+   -ms-flex: 1;
+   flex: 1.25;
+   border-radius: 15px;
+   margin: .5rem;
+   background-color: darkkhaki;
+   opacity: .8;
+}
+
+.left-column {
+   -webkit-flex: 1;
+   -ms-flex: 1;
+   flex: .75;
+   border-radius: 15px;
+   margin: .5rem;
+   background-color: darkkhaki;
+   opacity: .8;
+}
+
+/* Middle column */
+.center-column {
+  -webkit-flex: 2;
+  -ms-flex: 2;
+  flex: 2;
+  border-radius: 15px;
+  margin: .5rem;
+  background-color: #093708;
+  opacity: .8;
+  color: darkkhaki;
+}
+
+/* Style the footer */
+.footer {
+  background-color: #f1f1f1;
+  padding: 10px;
+  text-align: center;
+}
+
+.home-container {
+  background-image: url("../assets/goldenGolf.jpeg");
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  text-align: center;
+  font-family: 'Hedvig Letters Serif', serif;
+  color: #093708;
+  font-weight: 600;
+
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media (max-width: 600px) {
+  .row {
+    -webkit-flex-direction: column;
+    flex-direction: column;
+  }
+}
+
+</style>
+
+
+<!-- TODO: Include links/show:
   - leader board
   - tee-times
   - create a new league
@@ -32,26 +122,16 @@ TODO: IF LEAGUE ORGANIZER
 
 TODO: IF ADMIN
   - include ability/form to add a golf course -->
-</template>
+
 
 <script>
-// import LeaderBoard from "../components/HomePage";
-
 export default {
-  methods: {
-    // components: {
-    //   LeaderBoard,
-    // },
-    logOut(){
-      this.$store.commit("LOGOUT");
-      this.$router.push("/");
-    },
-  },
 };
-</script> 
+</script>
 
 
-<style scoped>
+
+<!-- <style scoped>
 .home {
   justify-content: center;
   align-items: center;
@@ -69,4 +149,4 @@ export default {
   color: #093708;
   font-weight: 600;
 }
-</style>
+</style> -->
