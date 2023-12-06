81a24a039eadec3040dc245865aa3481cbfc9331
James Marghella[D[D[D[D[D[D[D[D[D[D[D[i[C[3~[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C Marghella <j.marghella@gmail.com>
2023-12-06 13:36:03 -0500

    leaderboard N'at

--- vue/src/components/HomePage.vue
+++ vue/src/components/HomePage.vue
@@ -13,10 +13,15 @@ export default {
         return {
             username: "User321", // get info from db/login
             users: [
-                {name: "Soul Snatcher", score: 70, handicap: 8, course: ""},
-                {name: ""}
-            ]
-        }
-    }
-}
+                {name: "Soul Snatcher", score: 70, handicap: 8, course: "Green Acres"},
+                {name: "Two Thumbs Tony", score: 56, handicap: 5, course: "Your Mom's Yard"},
+                {name: "Bugz Almost", score: 63, handicap: 7, course: "Left field"},
+                // add as they get populated
+            ],
+        };
+    },
+    components: {
+        LeaderBoard,
+    },
+};
 </script>
