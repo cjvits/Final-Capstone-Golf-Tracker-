6f18e557334e5123c06b6839ea114d5fb1225391
Caleb Vits <calebvits@gmail.com>
2023-12-14 12:00:12 -0500

    and admin page formatting updates

--- vue/src/views/AdminView.vue
+++ vue/src/views/AdminView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="admin-page-container" v-if="this.$store.state.user.authorities.some((obj) => obj.name === 'ROLE_ADMIN')" >
-        Administrator Page
+        <h2 class="header">Administrator Page</h2>
         <NewCourseForm></NewCourseForm>
     </div>
     <div>
@@ -40,6 +40,24 @@
 
 .admin-page-container {
     background-color: darkkhaki;
+    padding-top: 1%;
+        background-image: url("../assets/goldenGolf.jpeg");
+        background-color: #312917;
+        height: 100vh;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        
+        font-family: 'Hedvig Letters Serif', serif;
+        color: #06612f;
+        font-weight: 600;
+        
+    
 }
+
+.header {
+        padding: 30px;
+        text-align: center;
+        font-size: 35px;
+    }
 
 </style>
